510f9770827f7fc0010f0b4d0156a6c885abb24b
Artyom Borisov <2001raf@gmail.com>
2023-03-08 21:51:29 +0500

    Added the script for setting up four cameras on a screen.

---
+++ Assets/Scripts/Cameras/FourCamerasView.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using MouseButton = UnityEngine.UIElements.MouseButton;
+
+public class FourCamerasView : MonoBehaviour
+{
+    private bool _isEnabled;
+    public Camera[] cameras = new Camera[4];
+
+    public void EnableView()
+    {
+        foreach (var cam in cameras)
+        {
+            cam.enabled = true;
+        }
+        cameras[0].rect = new Rect(0, 0, 0.5f, 0.5f);
+        cameras[1].rect = new Rect(0.5f, 0, 0.5f, 0.5f);
+        cameras[2].rect = new Rect(0, 0.5f, 0.5f, 0.5f);
+        cameras[3].rect = new Rect(0.5f, 0.5f, 0.5f, 0.5f);
+        _isEnabled = true;
+    }
+
+    public void DisableView()
+    {
+        foreach (var cam in cameras)
+        {
+            cam.enabled = false;
+        }
+        _isEnabled = false;
+    }
+}
