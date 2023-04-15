b97364857dd9b2a211cb86a931e452bd3a08b6a0
Максимов Александр Александрович <aa.maksimov@skbkontur.ru>
2023-04-15 15:49:55 +0500

    add untracked files

---
+++ Assets/Scripts/Player/InputHelper.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Player
+{
+	public class InputHelper : MonoBehaviour
+	{	
+		public InputSystemAsset InputActionAsset;
+		
+		private static readonly Dictionary<InputAction, KeyCode> DefaultControls = new Dictionary<InputAction, KeyCode>
+		{
+			[InputAction.Forward] = KeyCode.W,
+			[InputAction.Back] = KeyCode.S,
+			[InputAction.Left] = KeyCode.A,
+			[InputAction.Right] = KeyCode.D,
+			[InputAction.Jump] = KeyCode.Space,
+			[InputAction.Shot] = KeyCode.Mouse0,
+			[InputAction.Walk] = KeyCode.LeftShift,
+			[InputAction.Interact] = KeyCode.E,
+		};
+
+		private static readonly Dictionary<InputAction, KeyCode> CurrentControls = new Dictionary<InputAction, KeyCode>();
+
+		public static void SaveControls()
+		{
+			foreach (InputAction inputAction in Enum.GetValues(typeof(InputAction)))
+			{
+				PlayerPrefs.SetInt(inputAction.ToString(), (int) CurrentControls[inputAction]);
+			}
+			PlayerPrefs.Save();
+		}
+
+		public static void LoadControls()
+		{
+			foreach (InputAction inputAction in Enum.GetValues(typeof(InputAction)))
+			{
+				if (PlayerPrefs.HasKey(inputAction.ToString()))
+					CurrentControls[inputAction] = (KeyCode) PlayerPrefs.GetInt(inputAction.ToString());
+				else
+					CurrentControls[inputAction] = DefaultControls[inputAction];
+			}
+		}
+
+		public static bool GetKey(InputAction inputAction) => Input.GetKey(CurrentControls[inputAction]);
+		public static bool GetKeyDown(InputAction inputAction) => Input.GetKeyDown(CurrentControls[inputAction]);
+	}
+
+	public enum InputAction
+	{
+		Forward,
+		Back,
+		Left,
+		Right,
+		Jump,
+		Shot,
+		Walk,
+		Interact,
+		// DarkVision - может быть полезно, если охотник сможет с чем то взаимодействовать
+	}
+}
---
+++ Assets/Scripts/SymbolInserter/CustomReadWriteFunctions.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using Mirror;
+using UnityEngine;
+
+public static class CustomReadWriteFunctions
+{
+	public static void WriteSymbolButton(this NetworkWriter writer, SymbolButton symbolButton)
+	{
+		var c = symbolButton.SymbolInserter.GetComponent<NetworkIdentity>();
+		writer.WriteNetworkIdentity(c);
+		Debug.Log(c.netId);
+		writer.WriteInt((int) symbolButton.ButtonType);
+		Debug.Log(symbolButton.ButtonType);
+	}
+	
+	public static SymbolButton ReadSymbolButton(this NetworkReader reader)
+	{
+		NetworkIdentity networkIdentity = reader.ReadNetworkIdentity();
+		ButtonType buttonType = (ButtonType) reader.ReadInt();
+		var symbolButton = networkIdentity.GetComponentsInChildren<SymbolButton>().First(btn => btn.ButtonType == buttonType);
+		return symbolButton;
+	}
+}
