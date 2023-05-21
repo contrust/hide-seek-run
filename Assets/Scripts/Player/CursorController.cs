a396e440284983d16608b621f095f3e0f1abcfb8
SergeyBirukov <birukovsergej121@gmail.com>
2023-05-21 17:37:22 +0500

    Added onDisabled to CursorController.cs

--- Assets/Scripts/Player/CursorController.cs
+++ Assets/Scripts/Player/CursorController.cs
@@ -35,5 +35,11 @@ namespace Player
                 Cursor.lockState = CursorLockMode.Locked;
             }
         }
+        
+        private void OnDisable()
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
     }
 }
