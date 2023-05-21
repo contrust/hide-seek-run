633f7c0a93378d9b4905ca30ed4aa9265c2b41f6
lessy2014 <lessik03@gmail.com>
2023-05-21 18:42:51 +0500

    Slap effect test

--- Assets/SlapEffect.cs
+++ Assets/SlapEffect.cs
@@ -3,20 +3,46 @@ using System.Collections.Generic;
 using HUD.Effects;
 using UnityEngine;
 using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 public class SlapEffect : HUDEffect
 {
-    [SerializeField]private GameObject slap;
+    [SerializeField] private Image slap;
+    [SerializeField] private float duration;
+    [SerializeField] private float firstPartCoef;
+    private float secondPartCoef;
+    [SerializeField] private float scale;
+    
         
     public override void Show()
     {
+        secondPartCoef = 1 - firstPartCoef;
         StartCoroutine(ShowSlapEffect());
     }
         
     private IEnumerator ShowSlapEffect()
     {
-        slap.SetActive(true);
-        yield return new WaitForSeconds(0.5f);
-        slap.SetActive(false);
+        var defaultSize = slap.rectTransform.sizeDelta;
+        var sizeStep = defaultSize * scale;
+        var firstPart = duration * firstPartCoef;
+        var secondPart = duration * secondPartCoef;
+        var time = 0f;
+        slap.enabled = true;
+        while (time < firstPart)
+        {
+            slap.rectTransform.sizeDelta += sizeStep;
+            time += Time.deltaTime;
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+
+        time = 0;
+        while (time < secondPart)
+        {
+            slap.rectTransform.sizeDelta -= 2 * sizeStep;
+            time += Time.deltaTime;
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+
+        slap.enabled = false;
     }
 }
