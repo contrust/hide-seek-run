4916e6c5cfcf24371acafd9e0a31562c21d9ce7e
SergeyBirukov <72649075+SergeyBirukov@users.noreply.github.com>
2023-10-29 20:27:55 +0500

    Inserted symbols counter (#10)

---
+++ Assets/Scripts/HUD/InsertedSymbolsCounter.cs
@@ -0,0 +1,25 @@
+using TMPro;
+using UnityEngine;
+
+public class InsertedSymbolsCounter : MonoBehaviour
+{
+    public SymbolManager symbolManager;
+    public TextMeshProUGUI textMesh;
+    private const string ValueTemplate = "Введено символов: {count}";
+    private int count = -1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateTextIfValueChanged();
+    }
+
+    private void UpdateTextIfValueChanged()
+    {
+        if (count != symbolManager.CurrentCorrectInsertions)
+        {
+            count = symbolManager.CurrentCorrectInsertions;
+            textMesh.text = ValueTemplate.Replace("{count}", count.ToString());
+        }
+    }
+}
