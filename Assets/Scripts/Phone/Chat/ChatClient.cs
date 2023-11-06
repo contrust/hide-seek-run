82de0c460a8da500dc33fc342f288dad1e52e72d
SergeyBirukov <72649075+SergeyBirukov@users.noreply.github.com>
2023-11-06 16:28:07 +0500

    Chat refactoring (#21)

--- Assets/Scripts/Phone/Chat/ChatClient.cs
+++ Assets/Scripts/Phone/Chat/ChatClient.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using JetBrains.Annotations;
 using Mirror;
 using UnityEngine;
 using UnityEngine.Events;
@@ -9,19 +8,21 @@ namespace Phone.Chat
 {
     public class ChatClient: MonoBehaviour
     {
-        public List<Material> symbols;
         public int id;
-
+        public SymbolsContainer symbolsContainer;
         private ChatServer server;
-        [SerializeField] private Message message1;
-        [SerializeField] private Message message2;
-        [SerializeField] private Message message3;
+        [SerializeField] private List<Message> messages;
         [SerializeField] private string username;
         public UnityEvent<string, int> onReceiveMessage = new();
 
+        public void SendMessage(int symbolId)
+        {
+            server.CmdSendMessage(username, symbolId);
+        }
+
         private void Start()
         {
-            onReceiveMessage.AddListener(UpdateMessageQueue);
+            onReceiveMessage.AddListener(UpdateMessages);
             StartCoroutine(ConnectToChatServer());
         }
 
@@ -57,23 +58,15 @@ namespace Phone.Chat
             return localPlayer.steamName;
         }
 
-        public void SendMessage(int symbolId)
+        private void UpdateMessages(string senderName, int symbolId)
         {
-            server.CmdSendMessage(username, symbolId);
-        }
+            for (var i = 1; i < messages.Count; i++)
+            {
+                Debug.Log($"i = {i}");
+                messages[i - 1].CopyValues(messages[i]);
+            }
 
-        public void UpdateMessageQueue(string senderName, int symbolId)
-        {
-            CopyMessage(message2, message1);
-            CopyMessage(message3, message2);
-            message3.symbol.material = symbols[symbolId];
-            message3.sender.text = senderName;
-        }
-
-        private void CopyMessage(Message from, Message to)
-        {
-            to.sender.text = from.sender.text;
-            to.symbol.material = from.symbol.material;
+            messages[^1].SetValues(senderName, symbolId);
         }
     }
 }
--- Assets/Scripts/Phone/Chat/Message.cs
+++ Assets/Scripts/Phone/Chat/Message.cs
@@ -7,5 +7,18 @@ namespace Phone.Chat
     {
         public MeshRenderer symbol;
         public TextMeshPro sender;
+        public SymbolsContainer symbolsContainer;
+
+        public void SetValues(string senderName, int symbolId)
+        {
+            symbol.material = symbolsContainer.GetSymbol(symbolId);
+            sender.text = senderName;
+        }
+
+        public void CopyValues(Message from)
+        {
+            symbol.material = from.symbol.material;
+            sender.text = from.sender.text;
+        }
     }
 }
