b84c3bb9a69b9a0ecd9d34a75ddb7f0ea7c8377e
Artyom Borisov <2001raf@gmail.com>
2024-01-14 20:05:57 +0500

    Add bite ability to dog

--- Assets/Scripts/Maze/Dog.cs
+++ Assets/Scripts/Maze/Dog.cs
@@ -1,28 +1,90 @@
+using Mirror;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class Dog: MonoBehaviour
+public class Dog: RequireInstance<DogArea>
 {
     public MovableAgent agent;
+    private Victim victim;
     public float biteDistance;
+    public int biteDamage;
+    public float bitePeriod;
+    private float biteCooldown;
     public UnityEvent onBite = new UnityEvent();
+
+    private bool CanReachVictim()
+    {
+        return agent.GetDistance() < biteDistance;
+    }
+
+    private bool IsBiteCooldownPositive()
+    {
+        return biteCooldown <= 0;
+    }
+
+    private bool ShouldBite()
+    {
+        return agent.IsActive() &&
+               CanReachVictim() && 
+               IsBiteCooldownPositive();
+    }
 
     private void Start()
     {
         onBite.AddListener(LogBite);
+        onBite.AddListener(BiteVictim);
     }
 
     private void Update()
     {
-        BiteIfClose();
+        UpdateBiteCooldown();
+        if (ShouldBite())
+        {
+            Bite();
+        }
     }
 
-    private void BiteIfClose()
+    private void UpdateBiteCooldown()
     {
-        if (agent.IsActive() && agent.GetDistance() < biteDistance)
+        if (biteCooldown > 0)
         {
-            onBite.Invoke();
+            biteCooldown -= Time.deltaTime;
         }
+    }
+
+    private void Bite()
+    {
+        onBite.Invoke();
+    }
+
+    public Victim GetVictim()
+    {
+        return victim;
+    }
+
+    public void SetVictim(Victim victim)
+    {
+        this.victim = victim;
+        this.victim.onDeath.AddListener(UnsetVictim);
+        agent.SetTarget(this.victim.transform);
+    }
+
+    public void UnsetVictim()
+    {
+        agent.UnsetTarget();
+        victim = null;
+    }
+
+    public void BiteVictim()
+    {
+        victim.GetDamage(biteDamage, transform);
+        victim.GetStun();
+        ResetBiteCooldown();
+    }
+
+    private void ResetBiteCooldown()
+    {
+        biteCooldown = bitePeriod;
     }
 
     private void LogBite()
