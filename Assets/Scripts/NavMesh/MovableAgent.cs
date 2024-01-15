7af1110e329ab1309031f9ffc6e4b33d11b0da5b
Artyom Borisov <2001raf@gmail.com>
2024-01-16 02:19:45 +0500

    Change IsActive behaviour

--- Assets/Scripts/NavMesh/MovableAgent.cs
+++ Assets/Scripts/NavMesh/MovableAgent.cs
@@ -25,7 +25,7 @@ public class MovableAgent : MonoBehaviour
 
     public bool IsActive()
     {
-        return  !(target is null || agent.isStopped);
+        return  !(target is null) && !agent.isStopped;
     }
 
     public void SetTarget(Transform target)
@@ -66,7 +66,6 @@ public class MovableAgent : MonoBehaviour
         agent.CalculatePath(target.position, path);
         if (path.status != NavMeshPathStatus.PathComplete)
         {
-            Debug.Log(path.status);
             return;
         }
 
