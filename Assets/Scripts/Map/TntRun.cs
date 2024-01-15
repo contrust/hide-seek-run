009bfee2b2d14fd14ebde3632f0aa794618d7735
Alexander Maximov <56623309+Axonny@users.noreply.github.com>
2024-01-15 18:17:14 +0500

    Teleports (#43)
    * add teleports
    
    * update teleport model
    
    * update mat
    
    * Блум
    
    * zone
    
    * Фикс блума
    
    ---------
    
    Co-authored-by: lessy2014 <lessik03@gmail.com>

---
+++ Assets/Scripts/Map/TntRun.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class TntRun : MonoBehaviour
+{
+    [SerializeField] private float fallDistance = 2f;
+    [SerializeField] private float fallTime = 1f;
+    [SerializeField] private float waitTime = 3f;
+    [SerializeField] private float restoreTime = 5f;
+
+    private Coroutine fallBlockCoroutine;
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.name);
+        if (other.GetComponent<Victim>())
+        {
+            fallBlockCoroutine ??= StartCoroutine(FallBlock());
+        }
+    }
+
+    private IEnumerator FallBlock()
+    {
+        var material = gameObject.GetComponent<MeshRenderer>().material;
+        var boxCollider = gameObject.GetComponent<BoxCollider>();
+        var t = 0f;
+        var position = transform.position;
+        var startPos = position;
+        var endPos = position + Vector3.down * fallDistance;
+
+        yield return new WaitForSeconds(waitTime);
+        while (t <= 1)
+        {
+            transform.position = Vector3.Lerp(startPos, endPos, t);
+            material.color = material.color.WithAlpha(Mathf.Lerp(1, 0, t));
+            t += Time.deltaTime / fallTime;
+            yield return null;
+        }
+        boxCollider.enabled = false;
+        yield return new WaitForSeconds(restoreTime);
+
+        transform.position = startPos;
+        material.color = material.color.WithAlpha(1);
+        boxCollider.enabled = true;
+        fallBlockCoroutine = null;
+    }
+}
